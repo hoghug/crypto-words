1f3d65d8c7b59f0b7779c7bd8178e7a1b507afc7
Chris Lind <clind714@gmail.com>
2014-03-29 12:00:22 -0700

    adds functionality for a string of words

--- crypto.rb
+++ crypto.rb
@@ -1,27 +1,39 @@
-def crypto (shift, string)
+def phrase_builder (shift, string)
 
-	orig_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
-	shifted_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
-	word_to_change = string.split('')
-	crypto_word = []
+	phrase = string.split(' ')
+	crypto_phrase = []
+
+	@orig_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+	@shifted_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
 
 	looper = 0
 	while looper < shift.abs
 		if shift < 0
-			shifted_alpha.unshift(shifted_alpha.pop)
+			@shifted_alpha.unshift(@shifted_alpha.pop)
 		else 
-			shifted_alpha << shifted_alpha.shift
+			@shifted_alpha << @shifted_alpha.shift
 		end
 		looper += 1
 	end
 
-	word_to_change.each_with_index do |letter, index|
-		crypto_word << shifted_alpha[orig_alpha.index(letter)]
+	phrase.each do |word|
+		crypto_phrase << crypto_word(word)
 	end
 
-	string + ' = ' + crypto_word.join('')
+	crypto_phrase.join(' ')
+	
+end
 
+def crypto_word (word)
+	word_to_change = word.split('')
+	changed_word = []
+
+	word_to_change.each_with_index do |letter, index|
+		changed_word << @shifted_alpha[@orig_alpha.index(letter)]
+	end
+
+	changed_word.join('')
 end
 
 
-puts crypto(1, "child")
+puts phrase_builder(1, "child ban")
