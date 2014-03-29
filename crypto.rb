b291b096c5770d0d756780d6f7aa0369b5b71761
Chris Lind <clind714@gmail.com>
2014-03-29 13:29:54 -0700

    injects symbols for vowels

--- crypto.rb
+++ crypto.rb
@@ -3,25 +3,40 @@ def phrase_builder (shift, string)
 	phrase = string.split(' ')
 	crypto_phrase = []
 
-	@orig_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
-	@shifted_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+	orig_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+	shifted_alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+	vowels = ['a','e','i','o','u']
+	symbols = ['!','%','#','?','&']
 
 	looper = 0
 	while looper < shift.abs
 		if shift < 0
-			@shifted_alpha.unshift(@shifted_alpha.pop)
+			shifted_alpha.unshift(shifted_alpha.pop)
 		else 
-			@shifted_alpha << @shifted_alpha.shift
+			shifted_alpha << shifted_alpha.shift
 		end
 		looper += 1
+	end
+
+	@alpha_hash = {}
+
+	orig_alpha.each_with_index do |letter, index|
+		@alpha_hash[orig_alpha[index]] = shifted_alpha[index]
+	end
+
+	vowel_loop = 0
+	@alpha_hash.each_pair do |k,v|
+		if vowels.include?(v)
+			@alpha_hash[k] = symbols[vowel_loop]
+			vowel_loop += 1
+		end
 	end
 
 	phrase.each do |word|
 		crypto_phrase << crypto_word(word)
 	end
-
-	crypto_phrase.join(' ')
 	
+	crypto_phrase.join(' ')
 end
 
 def crypto_word (word)
@@ -29,11 +44,11 @@ def crypto_word (word)
 	changed_word = []
 
 	word_to_change.each_with_index do |letter, index|
-		changed_word << @shifted_alpha[@orig_alpha.index(letter)]
+		changed_word << @alpha_hash[letter]
 	end
 
 	changed_word.join('')
 end
 
 
-puts phrase_builder(1, "child ban")
+puts phrase_builder(1, "bat man")
